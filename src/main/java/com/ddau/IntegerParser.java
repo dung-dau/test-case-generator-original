9b093af2a66459ec02d1fe76532d0d38294a0229
Dung <dudau0924222@gmail.com>
2022-10-11 22:31:36 -0400

    refactor IntegerParser and add dynamic error string creation

--- src/main/java/com/ddau/IntegerParser.java
+++ src/main/java/com/ddau/IntegerParser.java
@@ -1,11 +1,14 @@
 package src.main.java.com.ddau;
 
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
 
 public class IntegerParser implements Parsable {
     private Integer[] data;
     private Scanner scanner;
-    String output = "";
+    private String output = "";
 
     IntegerParser(Scanner scanner, Integer[] data) {
         this.scanner = scanner;
@@ -14,32 +17,92 @@ public class IntegerParser implements Parsable {
     }
 
     public void parse() {
-        String choice = "q";
-        int n = data.length;
+        String choice;
 
-        if (n == 1) {
+        if (data.length == 1) {
             output += Integer.toString(data[0]);
         }
 
-        while (!choice.equals("c") && !choice.equals("a")) {
-            System.out.println(
-                    "How would you like your data to be formatted? (A/a: Java Array, C/c: Comma Separated List)");
-            choice = scanner.next().toLowerCase();
-            if (!choice.equals("c") && !choice.equals("a")) {
-                System.out.println(choice + " is not equal to \"c\" or \"j\"");
-                System.out.println("Please enter either A or a for Java Array or C or c for a Comma Separated List");
-            }
-        }
+        choice = getFormatChoice();
+        convertToCommaSeparatedList();
+
         if (choice.equals("a")) {
-            output += "{ ";
+            output = addFormattingCharacters("{ ", " }");
         }
-        for (int i = 0; i < n - 1; i++) {
+    }
+
+    private String getFormatChoice() {
+        String choice = "q";
+        ArrayList<Map.Entry<String, String>> formats = new ArrayList<>();
+        formats.add(new AbstractMap.SimpleImmutableEntry<String, String>("a", "Java Array"));
+        formats.add(new AbstractMap.SimpleImmutableEntry<String, String>("c", "Comma Separated List"));
+
+        while (search(formats, choice) < 0) {
+            System.out.println(
+                    "How would you like your data to be formatted?");
+            for (int i = 0; i < formats.size(); i++) {
+                System.out.println(formats.get(i).getKey() + ": " + formats.get(i).getValue());
+            }
+            System.out.print("Enter your option: ");
+            choice = scanner.next().toLowerCase();
+            System.out.println();
+            validateChoice(formats, choice);
+        }
+
+        return choice;
+    }
+
+    private void validateChoice(ArrayList<Map.Entry<String, String>> formats, String choice) {
+        int i = 0;
+        if (search(formats, choice) < 0) {
+            System.out.print(choice + " is not equal to ");
+            for (i = 0; i < formats.size() - 1; i++) {
+                System.out.print(formats.get(i).getKey().toUpperCase() + "/" + formats.get(i).getKey() + ", ");
+            }
+            System.out.println(formats.get(i).getKey().toUpperCase() + "/" + formats.get(i).getKey());
+
+            System.out.println(
+                    "Please enter either:");
+            for (i = 0; i < formats.size(); i++) {
+                System.out.println("\t" + formats.get(i).getKey().toUpperCase() + " or " + formats.get(i).getKey()
+                        + " for a " + formats.get(i).getValue());
+            }
+            System.out.println();
+        }
+    }
+
+    private int search(ArrayList<Map.Entry<String, String>> formats, String target) {
+        int low = 0, high = formats.size() - 1;
+        return binarySearch(formats, target, low, high);
+    }
+
+    private int binarySearch(ArrayList<Map.Entry<String, String>> formats, String target, int low, int high) {
+        if (low > high) {
+            return -1;
+        }
+
+        int mid = low + (high - low) / 2;
+        if (formats.get(mid).getKey().compareTo(target) == 0) {
+            return mid;
+        } else if (formats.get(mid).getKey().compareTo(target) > 0) {
+            return binarySearch(formats, target, low, mid - 1);
+        }
+        return binarySearch(formats, target, mid + 1, high);
+    }
+
+    private void convertToCommaSeparatedList() {
+        if (data.length < 2) {
+            return;
+        }
+
+        for (int i = 0; i < data.length - 1; i++) {
             output += Integer.toString(data[i]) + ", ";
         }
-        output += Integer.toString(data[n - 1]);
-        if (choice.equals("a")) {
-            output += " }";
-        }
+        output += Integer.toString(data[data.length - 1]);
+    }
+
+    private String addFormattingCharacters(String firstFormattingCharacters, String secondFormattingCharacters) {
+        return firstFormattingCharacters + output + secondFormattingCharacters;
     }
 
     public String getOutput() {
